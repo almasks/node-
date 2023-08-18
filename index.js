2f9aaca5acb82e0c113cf494a6aadf0e888865d9
Almas K S <mailmealmasks@gmail.com>
2023-08-18 22:37:01 +0530

    fs module with async await

--- index.js
+++ index.js
@@ -41,21 +41,21 @@
 //     }
 // })
 //fs /promise module
-const fs =require("node:fs/promises")
-fs.readFile("./file.txt","utf-8")
-.then(data=>console.log(data))
-.catch(error=>console.log(error))
-// using async and await
 // const fs =require("node:fs/promises")
-// async function readFile(){
-//     try{
-//         const data = await fs.readFile("./file.txt","utf-8")
-//         console.log(data)
-//     }catch(err){
-//         console.log(err)
-//     }
-// }
-// readFile()
+// fs.readFile("./file.txt","utf-8")
+// .then(data=>console.log(data))
+// .catch(error=>console.log(error))
+// using async and await
+const fs =require("node:fs/promises")
+async function readFile(){
+    try{
+        const data = await fs.readFile("./file.txt","utf-8")
+        console.log(data)
+    }catch(err){
+        console.log(err)
+    }
+}
+readFile()
 //using stream in fs module
 // const fs =require("node:fs")
 // const readableStream =fs.createReadStream("./file.txt",{
