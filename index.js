8d4c2bd16730758645d3aecbbbb99e1e24dd5a13
Almas K S <mailmealmasks@gmail.com>
2023-08-19 20:15:50 +0530

    server created

--- index.js
+++ index.js
@@ -57,17 +57,36 @@
 // }
 // readFile()
 //using stream in fs module
-const fs =require("node:fs")
-const zlib=require("node:zlib");
-const gzip =zlib.createGzip()
-const readableStream =fs.createReadStream("./file.txt",{
-    encoding:"utf-8"
-})
-readableStream.pipe(gzip).pipe(fs.WriteStream('./file2.txt.gz'))
-const writableStream =fs.createWriteStream("./file2.txt")
-readableStream.pipe(writableStream)
+// const fs =require("node:fs")
+// const readableStream =fs.createReadStream("./file.txt",{
+//     encoding:"utf-8"
+// })
+// const writableStream =fs.createWriteStream("./file2.txt")
+// // readableStream.pipe(writableStream)
 
 // readableStream.on("data", (chunk)=>{
 //     console.log(chunk)
 //     writableStream.write(chunk)
 // })
+// //using pipe 
+
+// const fs =require("node:fs")
+// const zlib=require("node:zlib");
+// const gzip =zlib.createGzip()
+// const readableStream =fs.createReadStream("./file.txt",{
+//     encoding:"utf-8"
+// })
+// readableStream.pipe(gzip).pipe(fs.WriteStream('./file2.txt.gz'))
+// const writableStream =fs.createWriteStream("./file2.txt")
+// readableStream.pipe(writableStream)
+//node server
+const http = require("node:http")
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':"text/plain"})
+    res.end("hello world welcome almas")
+    console.log(req)
+
+})
+server.listen(3004,()=>{
+    console.log("server created on port 3004")
+})
