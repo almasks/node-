1ae1ccf407c9c8c1428965ac8323799fd5b56c96
Almas K S <mailmealmasks@gmail.com>
2023-08-19 21:38:16 +0530

    http routing

--- index.js
+++ index.js
@@ -135,15 +135,41 @@
 //     console.log('server created on port 3002 with file from index')
 // })
 // ***************html templete**************
+// const http =require("node:http")
+// const fs=require("node:fs")
+// const server=http.createServer((req,res)=>{
+//     res.writeHead(200,{"Content-Type":"text:html"})
+//     const name="Almas"
+//     let html=fs.readFileSync("./index.html","utf-8")
+//     html=html.replace("{{name}}",name)
+//     res.end(html)
+// })
+// server.listen(3001,()=>{
+//     console.log("server created and html templete in port 3001")
+// })
+//*************http routing */*********** */
 const http =require("node:http")
-const fs=require("node:fs")
-const server=http.createServer((req,res)=>{
-    res.writeHead(200,{"Content-Type":"text:html"})
-    const name="Almas"
-    let html=fs.readFileSync("./index.html","utf-8")
-    html=html.replace("{{name}}",name)
-    res.end(html)
+const fs =require('node:fs')
+const server =http.createServer((req,res)=>{
+if(req.url==="/"){
+    res.writeHead(200,{"Content-Type":"text/plain"})
+    res.end("home page")
+}else if(req.url==="/about"){
+    res.writeHead(200,{"Content-Type":"text/plain"})
+    res.end("about page")
+}else if(req.url==="/api"){
+    res.writeHead(200,{"Content-Type":"application/json"})
+    res.end(JSON.stringify({
+        firstName:"Bruce",
+        lastName:"Wayne"
+    }))
+    
+}
+else{
+    res.writeHead(400)
+    res.end("no page has found")
+}
 })
 server.listen(3001,()=>{
-    console.log("server created and html templete in port 3001")
+    console.log("server portal created on 3001")
 })
